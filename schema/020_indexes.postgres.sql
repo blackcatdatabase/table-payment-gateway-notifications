b1b8e736c57dd9d0ef9e00a61c38ca5c21dc2e79
Vito416 <vito416@protonmail.com>
2025-12-05 22:05:23 +0100

    Fixed deep problems in schema, added safe checks not to repeat in future, using sha1 now in comment

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -1,7 +1,9 @@
--- Auto-generated from schema-map-postgres.yaml (map@74ce4f4)
+-- Auto-generated from schema-map-postgres.yaml (map@sha1:F0EE237771FBA8DD7C4E886FF276F91A862C3718)
 -- engine: postgres
 -- table:  payment_gateway_notifications
 
 CREATE UNIQUE INDEX IF NOT EXISTS ux_pg_notify_tenant_tx ON payment_gateway_notifications (tenant_id, transaction_id);
+
+CREATE INDEX IF NOT EXISTS idx_pg_notify_status_received ON payment_gateway_notifications (status, received_at);
 
 CREATE INDEX IF NOT EXISTS idx_pg_notify_tenant_status_received ON payment_gateway_notifications (tenant_id, status, received_at);
